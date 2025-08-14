a13caba9e6d8f093feab1bcf844370d585705426
Gopal Kumar Singh <gopalkumarsingh7007@gmail.com>
2025-08-14 09:27:44 +0530

    ...

---
+++ Typecasting/NarrowCasting.java
@@ -0,0 +1,11 @@
+package Typecasting;
+
+public class NarrowCasting {
+    public static void main(String[] args) {
+        double myDouble = 9.69;
+        int myInt = (int) myDouble;
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
